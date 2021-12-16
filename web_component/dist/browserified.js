cf983f9f81cea25b1f0e0e28e0c39aeb371dc98f
Andrija Petrovic <andrija.hers@gmail.com>
2021-12-16 18:03:19 +0100

    LoginFormLogic now supports "urlextraction" so that it can stay invisible and read window.location

--- web_component/dist/browserified.js
+++ web_component/dist/browserified.js
@@ -3,7 +3,10 @@ function createElements (execlib, applib, templatelib, htmltemplatelib) {
   'use strict';
 
   var lib = execlib.lib,
-    FormElement = applib.getElementType('FormElement');
+    FormElement = applib.getElementType('FormElement'),
+    usernamestring = '__remote__username',
+    passwordstring = '__remote__password',
+    loginbuttonfinder = 'loginbutton';
 
   function LoginFormLogic (id, options) {
     FormElement.call(this, id, options);
@@ -12,15 +15,50 @@ function createElements (execlib, applib, templatelib, htmltemplatelib) {
   LoginFormLogic.prototype.__cleanUp = function () {
     FormElement.prototype.__cleanUp.call(this);
   };
+  LoginFormLogic.prototype.show = function () {
+    var urlex = this.urlExtraction();
+    if (urlex) {
+      this.inputNamed(usernamestring).val(urlex[usernamestring]);
+      this.inputNamed(passwordstring).val(urlex[passwordstring]);
+      this.wantsSubmit.fire(urlex);
+      return;
+    }
+    FormElement.prototype.show.call(this);
+  };
   LoginFormLogic.prototype.fireInitializationDone = function () {
-    this.checkForInputNamed('__remote__username');
-    this.checkForInputNamed('__remote__password');
+    this.inputNamed(usernamestring);
+    this.inputNamed(passwordstring);
     FormElement.prototype.fireInitializationDone.call(this);
   };
-  LoginFormLogic.prototype.checkForInputNamed = function (name) {
-    if (this.$element.find("input[name='"+name+"']").length != 1) {
+  LoginFormLogic.prototype.inputNamed = function (name) {
+    var ret = this.$element.find("input[name='"+name+"']");
+    if (ret.length != 1) {
       throw new Error ('Login form has to have an input with name "'+name+'"');
     }
+    return ret;
+  };
+  LoginFormLogic.prototype.urlExtraction = function () {
+    var urlex = this.getConfigVal('urlextraction'), query, ret, username;
+    if (!urlex) {
+      return null;
+    }
+    if (!(usernamestring in urlex)) {
+      console.warn('No', usernamestring, 'in urlextraction');
+      return null;
+    }
+    if (!(passwordstring in urlex)) {
+      console.warn('No', passwordstring, 'in urlextraction');
+      return null;
+    }
+    query = (new URL(window.location)).searchParams;
+    username = query.get(urlex[usernamestring]);
+    if (!username) {
+      return null;
+    }
+    ret = {};
+    ret[usernamestring] = username;
+    ret[passwordstring] = query.get(urlex[passwordstring]);
+    return ret;
   };
   applib.registerElementType('LoginFormLogic', LoginFormLogic);
 }
